18e5cc34a76d18118dd977666694fca623bd36f0
arjunvmariyil <61988382+arjunvmariyil@users.noreply.github.com>
2020-03-12 11:54:15 -0500

    Module-5 checkin

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -19,17 +19,22 @@ namespace GradeBook.GradeBooks
             {
                 throw new InvalidOperationException("You must have at least 5 students to do ranked grading.");
             }
-
-            var threshold = (int)Math.Ceiling(Students.Count * 0.2);
+            // We need to seprate each bucket of students by 20% and sort their average through descending order in 20% bucket.
+            //So we have 10 students first 2 students will be in the first 20% bucket(garde A) next 20% bucket next 2 (grade b etc)
+            //Then we need to grade them accordingly. 
+            //In the case of 10 students Student with Highest and Second Highest grade will be stored in the sorted array gardes[0] and grades[1].
+            //Next in gardes[2] and grades[3].
+            // So if average greater than or equal to average in the last index of first 20% then we will give Grade A
+            var thresholdToSeparate = (int)Math.Ceiling(Students.Count * 0.2);
             var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
 
-            if (averageGrade >= grades[threshold - 1])
+            if (averageGrade >= grades[thresholdToSeparate - 1])
                 return 'A';
-            if (averageGrade >= grades[(threshold * 2) - 1])
+            if (averageGrade >= grades[(thresholdToSeparate * 2) - 1])
                 return 'B';
-            if (averageGrade >= grades[(threshold * 3) - 1])
+            if (averageGrade >= grades[(thresholdToSeparate * 3) - 1])
                 return 'C';
-            if (averageGrade >= grades[(threshold * 4) - 1])
+            if (averageGrade >= grades[(thresholdToSeparate * 4) - 1])
                 return 'D';
             return 'F';
         }
